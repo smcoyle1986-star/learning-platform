db29b9e22dbd7682d09ace6f3611779fdfef2722
smcoyle1986-star <smcoyle1986@gmail.com>
2026-08-14 23:09:17 +0900

    Fix Flashcards tray state synchronization

--- app/flashcards/page.tsx
+++ app/flashcards/page.tsx
@@ -18,7 +18,6 @@ import {
   GUEST_LESSON_TRAY_LIMIT,
   readLastSavedTray,
   readLessonTray,
-  subscribeToLessonTray,
   setEditingLessonSetId as persistEditingLessonSetId,
   writeLastSavedTray,
   writeLessonTray,
@@ -202,14 +201,6 @@ export default function FlashcardsPage() {
     if (!lessonTrayReady || authLoading) return;
     writeLessonTray(lessonTray as LessonCard[], user ? "account" : "guest");
   }, [authLoading, lessonTray, lessonTrayReady, user]);
-
-  useEffect(() => {
-    if (authLoading) return;
-    return subscribeToLessonTray(
-      (cards) => setLessonTray(cards as TrayItem[]),
-      user ? "account" : "guest",
-    );
-  }, [authLoading, user]);
 
   const {
     carouselState,
